a982f32c111752b1c0712732f8df620bb44ed5d5
jungo <81670100+jungo0@users.noreply.github.com>
2023-12-26 22:55:21 +0900

    feat:날짜,시간 버튼 기능

--- client/src/pages/createRoute/OptionFour.tsx
+++ client/src/pages/createRoute/OptionFour.tsx
@@ -15,21 +15,89 @@ import {
   PrevArrow,
   NextArrow,
 } from './OptionFour.style';
-
+import { useEffect, useRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 function OptionFour() {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [startTime, setStartTime] = useState<Date | null>(null);
+  const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisible] = useState(false);
+  const datePickerRef = useRef(null);
+  const timePickerRef = useRef(null);
+
+  const handleCalButtonClick = () => {
+    setDatePickerVisible(!isDatePickerVisible);
+    setTimePickerVisible(false);
+  };
+
+  const handleTimeButtonClick = () => {
+    setTimePickerVisible(!isTimePickerVisible);
+    setDatePickerVisible(false);
+  };
+
+  const handleCalButtonInputChange = () => {
+    // CalButton을 클릭하면 DatePicker 입력 칸이 나오도록 DatePicker를 열기
+    if (!isDatePickerVisible && datePickerRef.current) {
+      datePickerRef.current.setOpen(true);
+    }
+  };
+
+  const handleDateChange = (date: Date | null) => {
+    setStartDate(date);
+  };
+
+  const handleTimeChange = (time: Date | null) => {
+    setStartTime(time);
+  };
+
+  useEffect(() => {
+    if (isDatePickerVisible && datePickerRef.current) {
+      datePickerRef.current.setOpen(true);
+    }
+
+    if (isTimePickerVisible && timePickerRef.current) {
+      timePickerRef.current.setOpen(true);
+    }
+  }, [isDatePickerVisible, isTimePickerVisible]);
+
   return (
     <Container>
       <Title>출발지 정보를 입력해주세요</Title>
       <ButtonContainer>
         <PrevArrow src={prev_arrow} style={{ left: '-90px' }}></PrevArrow>
-        <CalButton>
-          <div style={{ fontSize: '1rem', color: 'var(--bright-gray)' }}>날짜 선택</div>
+        <CalButton onClick={handleCalButtonClick} onFocus={handleCalButtonInputChange}>
+          <div style={{ fontSize: '1rem', color: 'var(--bright-gray)' }}>
+            {startDate ? startDate.toLocaleDateString() : '날짜 선택'}
+          </div>
         </CalButton>
-        <TimeButton>
-          <div style={{ fontSize: '1rem', color: 'var(--bright-gray)' }}>시간 선택</div>
+        <TimeButton onClick={handleTimeButtonClick}>
+          <div style={{ fontSize: '1rem', color: 'var(--bright-gray)' }}>
+            {startTime ? startTime.toLocaleTimeString() : '시간 선택'}
+          </div>
         </TimeButton>
         <NextArrow src={next_arrow} style={{ right: '-90px' }}></NextArrow>
       </ButtonContainer>
+      {isDatePickerVisible && (
+        <DatePicker
+          selected={startDate}
+          onChange={handleDateChange}
+          dateFormat="yyyy-MM-dd"
+          ref={datePickerRef}
+          popperPlacement="bottom-start"
+        />
+      )}
+      {isTimePickerVisible && (
+        <DatePicker
+          selected={startTime}
+          onChange={handleTimeChange}
+          showTimeSelect
+          showTimeSelectOnly
+          timeCaption="시간"
+          dateFormat="h:mm aa"
+          ref={timePickerRef}
+        />
+      )}
       <InputBox placeholder="출발지 검색하기" />
       <ListContainer>
         <ListBox>
